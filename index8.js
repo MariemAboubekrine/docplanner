1926a8f4be07fb35dbf00d3366d8f4914ff4e21d
MariemAboubekrine <mariemaboubekrinediallo@gmail.com>
2024-06-23 14:24:31 +0000

    first commit

---
+++ index8.js
@@ -0,0 +1,11 @@
+alert('HELLO WORLD')  
+ var visitorName = prompt('what\'s your name:')
+ console.log(visitorName)
+ var isCoolWithIt = confirm("Are you ok with it?")
+ var a = 2 + 2 // I’m a single line comment
+/*
+And I’m a multi-lines comment,
+Anything in here will be ignored by the Javascript
+Interpreter.
+var b = 3 + 3
+*/
